dcbd447222e41ab14712c8fb1e7ab63e44d55554
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2024-05-24 20:37:29 +0800

    Ready files for upload.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
+import logo from './img/terabit.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="navbar frosted">
+        <div class="nav-section-logo">
+          <img src={logo} alt="" />
+        </div>
+        <div class="nav-section-links">
+          <a href="https://my.terabit.io/aff.php?aff=10" class="nav-link">Terabit</a>
+          <a href="https://github.com/Terabit-Desktop/tb_app" class="nav-link">GitHub</a>
+        </div>
+        <div class="nav-section-cta">
+          <a href="https://github.com/Terabit-Desktop/tb_app/releases/"><button class="btn-cta">Download</button></a>
+        </div>
+      </div>
+
+      <div class="content frosted">
+        <div class="content-hero">
+          <h1>Terabit Desktop</h1>
+          <p>Remote server management tools, directly on your desktop.</p>
+          <a href="https://github.com/Terabit-Desktop/tb_app/releases/"><button class="btn-cta">Download</button></a>
+        </div>
+      </div>
+    </>
   );
 }
 
